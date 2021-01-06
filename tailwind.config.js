8a172163847e29d49c48f80bf1810019bb4b80f9
gkp-dev <gkprofessionel@gmail.com>
2021-01-06 18:45:03 +0000

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+        extend: {
+            colors: {
+                "google-blue": "#4285f4",
+                "google-yellow": "#ea4335",
+                "google-red": "#fbbc05",
+                "google-green": "#34a853",
+                "google-fade": "#f8f9fa",
+            }
+        },
+    },
+    variants: {
+        extend: {},
+    },
+    plugins: [],
+}
